805e987133f601b6010abb5a296340ec1e5f7c9f
rharden132 <hardenx10@gmail.com>
2022-10-10 19:55:16 -0500

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SusanComponent } from './about/susan/susan.component';
+import { BobComponent } from './about/bob/bob.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    NavBarComponent,
+    SusanComponent,
+    BobComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
